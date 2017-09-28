a46407702422c0abb9a1fe4d383d06091dd5e7a5
rastamaha1998@gmail.com <artamoons@gmail.com>
2017-09-28 10:10:04 +0300

    20170928_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#4. piemers
+echo "atbilde uz jautajumu- kas sobrid tiek pildits? " $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits:"$#
+
+
+#3. piemers
+NAME="Vards Uzvards"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#2. piemers
+NAME="Vards Uzvards"
+#readonly NAME
+#echo $NAME
+NAME="Uzvards Vards"
+#echo $NAME
+
+#1. piemers
+NAME="Vards Uzvards"
+#echo $NAME
+
